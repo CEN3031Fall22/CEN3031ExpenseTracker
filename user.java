08c3025f4de751c6c8206344267254e3cfc71ae0
Alex Turner <112431308+aturn92@users.noreply.github.com>
2022-11-13 18:27:32 -0600

    Update user.java

--- user.java
+++ user.java
@@ -41,6 +41,62 @@ public class user {
 		userArray = new ArrayList<user>();
 	}
 	
+	public void loginMenu() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200,250);
+		frame.setVisible(true);
+		frame.setLayout(new BorderLayout(5,5));
+		
+		JButton menu0 = new JButton("Edit user Information");
+		JButton menu1 = new JButton("Add Expense");
+		JButton menu2 = new JButton("Budget Guidline");
+		JButton menu3 = new JButton("View Your Expenses");
+		JButton menu4 = new JButton("Exit Program");
+		
+		JPanel center = new JPanel();
+		center.setPreferredSize(new Dimension(35,100));
+		frame.add(center);
+		
+		center.add(menu0);
+		center.add(menu1);
+		center.add(menu2);
+		center.add(menu3);
+		center.add(menu4);
+		
+		menu0.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		menu1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		menu2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		menu3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		menu4.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		
+	}
+	
 	public int getUserIndex( ArrayList<user> u, String username) {
 		int index = -1;
 		for(int i = 0; i < u.size(); i++) {
