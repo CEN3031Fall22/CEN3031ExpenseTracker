17b8455566e550a29a272c1f6d8353af2224862d
waqarw97 <112781532+waqarw97@users.noreply.github.com>
2022-11-16 18:58:37 -0600

    Update tester.java

--- tester.java
+++ tester.java
@@ -7,8 +7,9 @@ public class tester {
 	public static void main(String[] args) {
 		//initial user object that is used to call all method 
 				//and create arraylist of user object
-		user user = new user();
-		mainMenu Mmenu = new mainMenu(user,user.userArray);
+//		user user = new user();
+		GuiClass newGui = new GuiClass();
+//		MainMenu menu = new MainMenu(user,user.userArray);
 				
 }
 }
