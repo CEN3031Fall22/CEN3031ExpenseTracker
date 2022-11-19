4114e0676780ae49f5e60317c46653905cf9caab
waqarw97 <112781532+waqarw97@users.noreply.github.com>
2022-11-18 19:29:43 -0600

    Update tester.java

--- tester.java
+++ tester.java
@@ -2,14 +2,18 @@ package sprint_1;
 
 
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class tester {
 
-	public static void main(String[] args) {
-		//initial user object that is used to call all method 
-				//and create arraylist of user object
-//		user user = new user();
-		GuiClass newGui = new GuiClass();
-//		MainMenu menu = new MainMenu(user,user.userArray);
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+
+		for (LookAndFeelInfo lafInfo : UIManager.getInstalledLookAndFeels()) {
+			System.out.println(lafInfo);
+		}
+		GuiClass newGui = new GuiClass("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 				
 }
 }
